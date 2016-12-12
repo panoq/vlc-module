5581e6c51a76740430699f8eee7f01cb9e38ac31
panoq <lauri.olavi.alho@gmail.com>
2016-12-12 19:50:41 +0000

    Config file changed

--- puppet/modules/vlc/manifests/init.pp
+++ puppet/modules/vlc/manifests/init.pp
@@ -1,11 +1,15 @@
 class vlc {
-	file {"/etc/gnome/defaults.list":
-		source => "/etc/puppet/modules/vlc/templates/defaults.list",
-	}
 
 	package {'vlc':
-		ensure => 'installed',
-		allowcdrom => 'true',
+                ensure => 'installed',
+                allowcdrom => 'true',
+        }
+
+	file {"/home/xubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/vlc.xml":
+		source => "/etc/puppet/modules/vlc/templates/vlc.xml",
+	}
+
+	tidy {"/home/xubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/parole.xml":
 	}
 
 }
